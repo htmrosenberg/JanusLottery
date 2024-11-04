4e51ecebe8cdae92acae49574ec54dff133e8636
Harold Rosenberg <htmrosenberg@gmail.com>
2024-11-04 20:59:39 +0000

    tst

--- test/BuyTickets.t.sol
+++ test/BuyTickets.t.sol
@@ -27,7 +27,7 @@ contract BuyTicksTest is Test {
     uint256 private ticketPrice;
     uint32 private maximumTickets;
     uint16 private sellingPeriodHours;
-
+    uint256 private jackpotValue;
     address public PLAYER = makeAddr("player");
     uint256 public constant STARTING_USER_BALANCE = 10 ether;
     uint256 public constant LINK_BALANCE = 100 ether;
@@ -55,14 +55,19 @@ contract BuyTicksTest is Test {
         vm.stopPrank();*/
 
         //make jackpot offer
-        uint256 jackpotValue = deployment.MINIMUM_JACKPOT();
-        ticketPrice = 10 wei;
-        maximumTickets = 10;
+        uint32 fundingHours = deployment.FUNDING_PERIOD_HOURS();
+        jackpotValue = deployment.MINIMUM_JACKPOT();
+        ticketPrice = 10 gwei;
+        maximumTickets = 5;
         sellingPeriodHours = deployment.MAXIMUM_SELLING_PERIOD_HOURS()-1;
-        janusLottery.jackPotOffer{ value : jackpotValue}(ticketPrice, maximumTickets, sellingPeriodHours);
-
-//        janusLottery.checkUpkeep(null);  
-//        janusLottery.performUpkeep()      
+        address funder = address(0x1);
+        vm.deal(funder, 2 ether);
+        vm.prank(funder);
+        janusLottery.jackPotOffer{value: jackpotValue}(ticketPrice, maximumTickets, sellingPeriodHours);
+        vm.warp(block.timestamp + (fundingHours * 60 * 60));
+        vm.roll(block.number + 1);
+        janusLottery.performUpkeep("");
+        assert(janusLottery.isSelling());
     }
 
 // ┌──────────────────────────────────────────┐
@@ -71,8 +76,86 @@ contract BuyTicksTest is Test {
 // │╹ ╹┗━┛   ╹ ╹┗━┛╹┗╸┗━╸   ┗━┛╹  ╹  ┗━╸╹┗╸┗━┛│
 // └──────────────────────────────────────────┘
 
-//test past offer phase
+    function testNoMoreJackpotOffers() public {
+        vm.expectRevert(JanusLottery.JanusLottery__NotInFundingState.selector);
+        janusLottery.jackPotOffer{value: jackpotValue}(ticketPrice, maximumTickets, sellingPeriodHours);       
+    }
 
+// ┌────────────────┐
+// │┏┓ ╻ ╻╻ ╻╻┏┓╻┏━╸│
+// │┣┻┓┃ ┃┗┳┛┃┃┗┫┃╺┓│
+// │┗━┛┗━┛ ╹ ╹╹ ╹┗━┛│
+// └────────────────┘
 
+    function testBuyingNotEnoughAmount() public {
+        address funder = address(0x5);
+        vm.deal(funder, 1 ether);
+        vm.prank(funder);
+
+        vm.expectRevert(JanusLottery.JanusLottery__InvalidTicketPrice.selector);
+        janusLottery.buyTicket{ value: ticketPrice - 1 gwei}();
+    }
+
+    function testBuyingTooMuchAmount() public {
+        address funder = address(0x5);
+        vm.deal(funder, 1 ether);
+        vm.prank(funder);
+
+        vm.expectRevert(JanusLottery.JanusLottery__InvalidTicketPrice.selector);
+        janusLottery.buyTicket{ value: ticketPrice + 1 gwei}();
+    }
+
+    function testCorrectAmount() public {
+        address funder = address(0x5);
+        vm.deal(funder, 1 ether);
+        vm.prank(funder);
+
+        janusLottery.buyTicket{ value: ticketPrice}();
+
+        assertEq(janusLottery.getTotalTicketHolders(),1);
+        assertEq(janusLottery.getTotalTicketsSale(),ticketPrice);
+        assertEq(janusLottery.getTicketHolder(0),address(0x5));
+    }
+
+    function testTooManyTickets() public {
+        address funder = address(0x5);
+        vm.deal(funder, 1 ether);
+ 
+        for (uint i = 0; i < 5; i++) {
+            vm.prank(funder);
+            janusLottery.buyTicket{ value: ticketPrice}();
+        }
+
+        assertEq(janusLottery.getTotalTicketHolders(),5);
+        assertEq(janusLottery.getTotalTicketsSale(),ticketPrice*5);
+        assertEq(janusLottery.getTicketHolder(0),address(0x5));
+        assertEq(janusLottery.getTicketHolder(1),address(0x5));
+        assertEq(janusLottery.getTicketHolder(2),address(0x5));
+        assertEq(janusLottery.getTicketHolder(3),address(0x5));
+        assertEq(janusLottery.getTicketHolder(4),address(0x5));
+
+        vm.expectRevert(JanusLottery.JanusLottery__SoldOut.selector);
+
+        vm.prank(funder);
+        janusLottery.buyTicket{ value: ticketPrice}();
+    }
+
+// ┌───────────────────────────────────────────────────────┐
+// │┏━╸╻  ┏━┓┏━┓┏━╸   ┏━┓┏━╸╻  ╻  ╻┏┓╻┏━╸   ┏━┓╻ ╻┏━┓┏━┓┏━╸│
+// │┃  ┃  ┃ ┃┗━┓┣╸    ┗━┓┣╸ ┃  ┃  ┃┃┗┫┃╺┓   ┣━┛┣━┫┣━┫┗━┓┣╸ │
+// │┗━╸┗━╸┗━┛┗━┛┗━╸   ┗━┛┗━╸┗━╸┗━╸╹╹ ╹┗━┛   ╹  ╹ ╹╹ ╹┗━┛┗━╸│
+// └───────────────────────────────────────────────────────┘
+
+    function testClosingSellingPhaseTooEarly() public {
+
+    }
+
+    function testClosingSellingPhaseWithoutTicketsSold() public {
+        
+    }
+
+    function testClosingSellingPhase() public {
+        
+    }
 
 }
